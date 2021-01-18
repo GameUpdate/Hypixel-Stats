1679b543d1296ca998998d25cb8127452193edf9
Game <gameupdate.paypal@gmail.com>
2021-01-18 22:26:14 +0100

    Setup

--- storage/newGuild.js
+++ storage/newGuild.js
@@ -4,27 +4,6 @@ const guild = new Schema({
     guildID: { type: String },
     bot_prefix: { type: String, default: '.' },
     color: { type: String, default: '#2F3136' },
-    leveling: {
-        nxtLevel: { type: Number, default: 100 },
-        xpMin: { type: Number, default: 20 },
-        xpMax: { type: Number, default: 35 },
-    },
-    welcome: {
-        enabled: { type: Boolean, default: false },
-        image: { type: Boolean, default: false },
-        channel: { type: String, default: '' },
-        message: { type: String, default: `Welcome {member.name} to {guild}!` }
-    },
-    goodbye: {
-        enabled: { type: Boolean, default: false },
-        image: { type: Boolean, default: false },
-        channel: { type: String, default: '' },
-        message: { type: String, default: `{member.name} left the server!` }
-    },
-    autorole: {
-        enabled: { type: Boolean, default: false },
-        role: { type: String, default: '' },
-    },
     reactionroles: {
         type: [{
             message: { type: String },
@@ -34,13 +13,20 @@ const guild = new Schema({
             role: { type: Object },
         }], default: []
     },
-    xprole: { type: Array, default: [] },
-    msgrole: { type: Array, default: [] },
-    cmds: { type: [[String, Boolean]], default: [] },
-    filter: { type: [String], default: [] },
-    logs: {
-        enabled: { type: Boolean, default: false },
-        channel: { type: String, default: '' },
+    giveaways: {
+        type: [{
+            message: { type: String },
+            channel: { type: String },
+            guild: { type: String },
+            prize: { type: String },
+            end: { type: String },
+        }], default: []
+    },
+    mutes: {
+        type: [{
+            userID: { type: String },
+            time: { type: String },
+        }], default: []
     },
 })
 module.exports = model("guilds", guild);
--- storage/newProfile.js
+++ storage/newProfile.js
@@ -3,17 +3,7 @@ const { Schema, model } = require("mongoose");
 const user = new Schema({
     guildID: { type: String },
     userID: { type: String },
-    lvl: {
-        level: { type: Number, default: 1 },
-        xp: { type: Number, default: 0 },
-        canXP: { type: Boolean, default: true },
-    },
-    eco: {
-        bal_changes: { type: [Number], default: [] },
-        bank: { type: Number, default: 0 },
-        gems: { type: Number, default: 1 },
-        bal: { type: Number, default: 5000 },
-    },
+    username: { type: String },
     pf: {
         msgTotal: { type: Number, default: 0 },
         firstJoin: { type: String, default: 'Couldn\'t find when' },
@@ -27,9 +17,6 @@ const user = new Schema({
         fakes: { type: Number, default: 0 },
         left: { type: Number, default: 0 },
     },
-    rooms: {
-        open: { type: Boolean, default: false },
-    }
 })
 module.exports = model("users", user);
 
